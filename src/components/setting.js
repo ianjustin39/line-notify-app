71161ed28a0d5e698feaf42287790bd6ef3c129f
ian <ianjustin39@gmail.com>
2020-08-15 16:29:17 +0800

    add setting checkbox status

--- src/components/setting.js
+++ src/components/setting.js
@@ -4,30 +4,49 @@ import { Form, Button, Col } from "react-bootstrap";
 
 
 function Setting() {
-  const [validated, setValidated] = useState(false);
+  const [token, setToken] = useState("");
+  const [serviceName, setServiceName] = useState("");
+  const [status, setStatus] = useState(false);
+
+  const test = (event) => {
+    const checkStatus = status ? false : true
+    setStatus(checkStatus)
+
+    console.log(checkStatus)
+  }
 
   const handleSubmit = (event) => {
+
+    if (!status) {
+      alert("請確認輸入資料正確並打勾");
+      return;
+    }
+    console.log('sss')
+
     const form = event.currentTarget;
-    alert(form.checkValidity());
+    
     if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
     }
 
-    setValidated(true);
   };
 
 
   return (
-      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Form noValidate onSubmit={handleSubmit}>
         <Form.Group controlId="formBasicEmail">
 
           <Form.Row>
             <Form.Label column="lg" lg={2}>Notify 名稱</Form.Label>
 
             <Col>
-              <Form.Control type="text" placeholder="Enter Name" />
-
+              <Form.Control 
+              type="text" 
+              placeholder="Enter Name" 
+              onChange={(event) => {
+                setServiceName(event.target.value);
+              }}/>
             </Col>
 
           </Form.Row>
@@ -38,14 +57,26 @@ function Setting() {
             <Form.Label column="lg" lg={2}>Notify Token</Form.Label>
 
             <Col>
-              <Form.Control type="password" placeholder="Enter Token" />
+              <Form.Control 
+              type="password" 
+              placeholder="Enter Token" 
+              onChange={(event) => {
+                setToken(event.target.value);
+              }}/>
 
             </Col>
 
           </Form.Row>
         </Form.Group>
         <Form.Group controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
+          <Form.Check 
+          required
+          type="checkbox" 
+          label="Check me out" 
+          onChange={(event) => {
+            test(event);
+            // setStatus(event.target.value);
+          }}/>
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
